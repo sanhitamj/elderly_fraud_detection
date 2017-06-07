8a0cb7f70cf72d15efbeb7e6ba82ce25cd1d5573
sanhitamj <sanhita.joshi@gmail.com>
2017-06-07 17:58:32 -0500

    data cleanup

--- expense_sqltocsv.py
+++ expense_sqltocsv.py
@@ -51,7 +51,11 @@ column_rename = {'IncomeExpenseReportId': 'IncExpId', 'CaseFileReportId': 'CasFi
                  'TransactionCategoryId': 'TranCat', 'CourtOrderTransaction': 'CourtOrdTran',
                  'ProtectedPersonLivesAtCareFacility': 'CarFac'}
 
+df.rename(columns=column_rename, inplace=True)
+
 df['Expense'] = df['IsExpense'].astype(int)
+df['CarFac'] = df['CarFac'].astype(int)
+df['CourtOrdTran'] = df['CourtOrdTran'].astype(int)
 
 del df['CreateUserId']
 del df['LastModUserId']
@@ -62,7 +66,10 @@ del df['TrustName']
 del df['IsIncome']
 del df['IsExpense']
 
-df.rename(columns=column_rename, inplace=True)
+
+# Remove transactions which are either 'Transfer to Trust' (id=245) or 'CAAP Audit
+# Expense Adjustment' (id=249)
+
 
 df2 = read_sql_table('TransactionCategories')
 
@@ -74,6 +81,7 @@ for i in xrange(len(trans_cat)):
 
 df['TranDescr'] = df['TranCat'].map(d_tran)
 
+df = df[(df['TranCat']!= 245) & (df['TranCat']!= 249)]
 
 columns_to_keep = ['IncExpId', 'CasFil', 'Amount', 'TranDt', 'TranCat', 'TranDescr', 'CourtOrdTran', 'CarFac' ]
 
@@ -82,7 +90,7 @@ columns_to_keep = ['IncExpId', 'CasFil', 'Amount', 'TranDt', 'TranCat', 'TranDes
 df = df[(df['Expense']==1) & (df['TranDt']> datetime.date(year=2010,month=1,day=1))
         & (df['TranDt']<datetime.date(2016, 1, 1))][columns_to_keep]
 
-df['NoDays'] = (df['TranDt'] - df['TranDt'].min())/ np.timedelta64(1,'D')
+df['NumDays'] = (df['TranDt'] - df['TranDt'].min())/ np.timedelta64(1,'D')
 
 df.to_csv('expense.csv')
 
@@ -91,3 +99,23 @@ For feature engineering
 '''
 
 df['TranDt'] = pd.to_datetime(df['TranDt'])
+
+duration = df.groupby(by='CasFil')['NumDays'].max() - df.groupby(by='CasFil')['NumDays'].min()
+NoCount = df.groupby(by='CasFil')['Amount'].count()
+Total = df.groupby(by='CasFil')['Amount'].sum()
+
+raw = {'Duration': list(duration), 'NumDataPts' : list(NoCount), 'Total' : Total}
+explore = pd.DataFrame(raw, columns = ['Duration', 'NumDataPts',  'Total'], index=NoCount.index)
+explore.reset_index(level=0, inplace=True)
+
+# Create a pivot dataframe to count number of transactions in each Category
+# If there is no trasaction of certain category, df_pivot.fillna(value=0)
+
+df_piv = df.pivot_table(values=['Amount'], index=['CasFil'], columns='TranCat', aggfunc='count')
+df_piv.fillna(0, inplace=True)
+
+# Add more columns to this pivot dataframe
+df_piv['CasFil'] = df_piv.index
+df_piv = pd.merge(df_piv, explore, how='outer', left_on='CasFil', right_on='CasFil')
+del df_piv[df_piv.columns.tolist()[-5]]
+df_piv.to_csv('expense_matrix.csv')
